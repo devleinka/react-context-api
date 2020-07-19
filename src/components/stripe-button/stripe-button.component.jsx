0a29eed080d596fdaabf9d9ee8ca6333984c286a
Lea Mikulcová <42700247+devleinka@users.noreply.github.com>
2020-07-19 22:20:56 +0200

    post fork init

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51Gzk4hIBG9JFjG9QFCpgWcpkQZbp7by0mhz9rUMBdfUUlzNbWQvjdsiBHNRI4KgGo0m1nwBzNr5MxkERiVg20QwD00ENaJDExT';
 
   const onToken = token => {
     console.log(token);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyB_tXU03IN59Z6YP0rud6ey9Mu9-30-C64",
+  authDomain: "ecommerce-demo-db-73c9b.firebaseapp.com",
+  databaseURL: "https://ecommerce-demo-db-73c9b.firebaseio.com",
+  projectId: "ecommerce-demo-db-73c9b",
+  storageBucket: "ecommerce-demo-db-73c9b.appspot.com",
+  messagingSenderId: "1093260398530",
+  appId: "1:1093260398530:web:ad12428df8f8d5f99fd05b"
 };
 
 firebase.initializeApp(config);
